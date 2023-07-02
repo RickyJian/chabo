3932a6e53daa0fca4111cc6135afa613e5904a76
RickyJian <rjricky03bc@gmail.com>
2023-07-02 19:21:35 +0800

    add app page

---
+++ lib/constant.dart
@@ -0,0 +1,10 @@
+class Constant {
+  Constant._();
+
+  /// application name
+  static const String appName = 'Chabo';
+
+  /// fab
+  static const double fabLeftMargin = 1.5;
+  static const double fabBottomMargin = 1.5;
+}
