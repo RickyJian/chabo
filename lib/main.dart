31ce9a21e98d26ca24aa2af881f6125f94b4f471
RickyJian <rjricky03bc@gmail.com>
2023-07-07 00:14:16 +0800

    add openhuninn font

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
           title: Constant.appName,
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
+            fontFamily: 'openhuninn',
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
             useMaterial3: true,
           ),
