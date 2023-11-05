455b65624d807c9f892ffa778d1b31dbb77b91bc
RickyJian <rjricky03bc@gmail.com>
2023-11-05 18:17:43 +0800

    add android get keyboard height

--- android/app/src/main/kotlin/github/com/chabo/MainActivity.kt
+++ android/app/src/main/kotlin/github/com/chabo/MainActivity.kt
@@ -1,6 +1,28 @@
 package github.com.chabo
 
+import android.graphics.Rect
+import android.view.View
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, "chabo/keyboard").setStreamHandler(object : EventChannel.StreamHandler {
+
+            // refer to: https://medium.com/@munnsthoughts/detecting-if-the-android-keyboard-is-open-using-kotlin-rxjava-2-8aee9fae262c
+            override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+                val rootView = activity.findViewById<View>(android.R.id.content)
+                window.decorView.rootView.viewTreeObserver.addOnGlobalLayoutListener {
+                    val rect = Rect().apply { rootView.getWindowVisibleDisplayFrame(this) }
+                    val screenHeight = rootView.height
+                    val keyboardHeight = screenHeight - rect.bottom
+                    events?.success(keyboardHeight)
+                }
+            }
+
+            override fun onCancel(arguments: Any?) {}
+        })
+    }
 }
