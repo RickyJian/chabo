5bae45dd3935b3df11a1324aa6349ad09d556a06
RickyJian <rjricky03bc@gmail.com>
2025-07-16 23:29:11 +0800

    add l10n settings

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,86 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Chabo';
+
+  @override
+  String get dayPeriodAM => 'AM';
+
+  @override
+  String get dayPeriodPM => 'PM';
+
+  @override
+  String get weekdaySUN => 'SUN';
+
+  @override
+  String get weekdayMON => 'MON';
+
+  @override
+  String get weekdayTUE => 'TUE';
+
+  @override
+  String get weekdayWED => 'WED';
+
+  @override
+  String get weekdayTHU => 'THU';
+
+  @override
+  String get weekdayFRI => 'FRI';
+
+  @override
+  String get weekdaySAT => 'SAT';
+
+  @override
+  String get alarm => 'Alarm';
+
+  @override
+  String get alarmSystem => 'System Alarms';
+
+  @override
+  String get dayShort => 'D';
+
+  @override
+  String get hourShort => 'H';
+
+  @override
+  String get minuteShort => 'M';
+
+  @override
+  String get hourLong => 'Hour';
+
+  @override
+  String get minuteLong => 'Minute';
+
+  @override
+  String get label => 'Label';
+
+  @override
+  String get labelEmpty => 'empty label';
+
+  @override
+  String get labelHint => 'input label';
+
+  @override
+  String get infoMsgWeekdayIsEmpty =>
+      'When week days are disabled, will set every day enabled.';
+
+  @override
+  String get cancel => 'cancel';
+
+  @override
+  String get save => 'save';
+
+  @override
+  String get clockCreateTitle => 'New Alarm Clock';
+
+  @override
+  String get setting => 'Setting';
+}
---
+++ lib/l10n/app_localizations_zh.dart
@@ -0,0 +1,85 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Chinese (`zh`).
+class AppLocalizationsZh extends AppLocalizations {
+  AppLocalizationsZh([String locale = 'zh']) : super(locale);
+
+  @override
+  String get appTitle => '日本矮雞';
+
+  @override
+  String get dayPeriodAM => '上午';
+
+  @override
+  String get dayPeriodPM => '下午';
+
+  @override
+  String get weekdaySUN => '日';
+
+  @override
+  String get weekdayMON => '一';
+
+  @override
+  String get weekdayTUE => '二';
+
+  @override
+  String get weekdayWED => '三';
+
+  @override
+  String get weekdayTHU => '四';
+
+  @override
+  String get weekdayFRI => '五';
+
+  @override
+  String get weekdaySAT => '六';
+
+  @override
+  String get alarm => '鬧鈴';
+
+  @override
+  String get alarmSystem => '系統鬧鈴';
+
+  @override
+  String get dayShort => '天';
+
+  @override
+  String get hourShort => '時';
+
+  @override
+  String get minuteShort => '分';
+
+  @override
+  String get hourLong => '小時';
+
+  @override
+  String get minuteLong => '分鐘';
+
+  @override
+  String get label => '標籤';
+
+  @override
+  String get labelEmpty => '未設定標籤';
+
+  @override
+  String get labelHint => '請輸入標籤';
+
+  @override
+  String get infoMsgWeekdayIsEmpty => '當星期皆為非啟用時，自動將每天設定為啟用。';
+
+  @override
+  String get cancel => '取消';
+
+  @override
+  String get save => '存檔';
+
+  @override
+  String get clockCreateTitle => '新增鬧鐘';
+
+  @override
+  String get setting => '設定';
+}
